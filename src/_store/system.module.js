519e0e4948ae275886913a8e4e956bd3b9d7e38b
Jonathan Cobb <jonathan@kyuss.org>
2020-01-17 10:11:58 -0500

    rename nginxPort to sslPort

--- src/_store/system.module.js
+++ src/_store/system.module.js
@@ -12,7 +12,7 @@ const state = {
         entityClasses: [],
         locales: ['en_US'],
         cloudConfigs: {},
-        nginxPort: null
+        sslPort: null
     },
     entityConfigs: {},
     searchResults: [],
