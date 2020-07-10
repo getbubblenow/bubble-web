33c83fdbfebcbaa7349bf78538855d5878cd5a75
Tyler Chen <tyler@noreply.git.bubblev.org>
2020-07-10 15:07:21 +0000

    Implement CSS preprocessor - SASS (#13)
    Merge branch 'master' into feat/sass-loader
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/sass-loader
    
    feat: implement SASS loader to webpack
    
    Co-authored-by: jonathan <jonathan@noreply.git.bubblev.org>
    Co-authored-by: Tyler <everdev0923@gmail.com>
    Reviewed-on: https://git.bubblev.org/bubblev/bubble-web/pulls/13

--- webpack.config.js
+++ webpack.config.js
@@ -6,46 +6,53 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.vue']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue?$/,
-                exclude: /(node_modules)/,
-                use: 'vue-loader'
-            },
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.(css|less)$/,
-                use: [{
-                    loader: "css-loader" // translates CSS into CommonJS
-                }]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new CopyWebpackPlugin([
-            { from: 'src/public', to: '' }
-        ])
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue?$/,
+        exclude: /(node_modules)/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
-    devServer: {
-        historyApiFallback: true
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            production: true,
-            apiUrl: '/api'
-        })
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin([{ from: 'src/public', to: '' }]),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    // proxy: {
+    //     '/api': 'http://beta.bubv.net:8888'
+    // }
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      production: true,
+      apiUrl: '/api',
+    }),
+  },
 };
