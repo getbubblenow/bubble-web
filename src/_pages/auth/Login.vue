96b89a0365ee269f9b92176c8d9d700203399101
Tyler Chen <tyler@noreply.git.bubblev.org>
2020-08-11 02:33:14 +0000

    MFA integration (#36)
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: implement MFA in login
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: implement adding ssh key
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    fix: showing default values
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: implement selector placeholders and default values
    
    Merge branch 'feat/ui-layout' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    fix: getting user information after setting payment method
    
    Merge branch 'master' into feat/ui-layout
    
    feat: implement setting payment plan in payment page
    
    feat: populate options for advanced settings modal
    
    fix: remove showing advanced settings modal when loading the page
    
    feat: implement Advanced Settings Modal layout
    
    feat: implement modal and selector components
    
    feat: implement launch bubble screen
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: implement add stripe card
    
    feat: implement login process
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: implement payment page
    
    feat: implement mail verification screen
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: prevent registration when the configs.allowRegistration is set to false
    
    fix: weird routing
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: implement actual pages
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: api integration for register page
    
    fix: messages
    
    feat: api integration for forget-password page
    
    fix: login title and password placeholder
    
    feat: disable button until get response
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: integrate api for new login page
    
    feat: implement responsive header
    
    feat: implement checkbox
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    fix: checkbox event issue
    
    feat: implement register page
    
    feat: implement registration page
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: implement UI for forgot-password
    
    Merge branch 'master' of git.bubblev.org:bubblev/bubble-web into feat/ui-layout
    
    feat: implement new login page
    
    feat: implement input shared component
    
    Co-authored-by: Tyler <everdev0923@gmail.com>
    Co-authored-by: jonathan <jonathan@noreply.git.bubblev.org>
    Reviewed-on: https://git.bubblev.org/bubblev/bubble-web/pulls/36

--- src/_pages/auth/Login.vue
+++ src/_pages/auth/Login.vue
@@ -22,18 +22,11 @@
         <Input
           class="form-control"
           :class="{
-            'is-invalid': submitted && emailErrors && emailErrors.length,
+            'is-invalid': submitted && errors.has('name'),
           }"
           v-model="email"
-          @input="$v.email.$touch()"
           :placeholder="messages.field_email_hint"
         />
-        <span
-          class="form-error"
-          v-if="submitted && emailErrors && emailErrors.length"
-        >
-          {{ emailErrors.join(', ') }}
-        </span>
         <div
           v-if="submitted && errors.has('account')"
           class="invalid-feedback d-block"
@@ -52,18 +45,11 @@
           class="form-control"
           type="password"
           :class="{
-            'is-invalid': submitted && passwordErrors && passwordErrors.length,
+            'is-invalid': submitted && errors.has('password'),
           }"
           v-model="password"
-          @input="$v.password.$touch()"
           :placeholder="messages.field_label_enter_password"
         />
-        <span
-          class="form-error"
-          v-if="submitted && passwordErrors && passwordErrors.length"
-        >
-          {{ passwordErrors.join(', ') }}
-        </span>
         <div
           v-if="submitted && errors.has('password')"
           class="invalid-feedback d-block"
@@ -76,18 +62,11 @@
         <Input
           class="form-control"
           :class="{
-            'is-invalid': submitted && totpErrors && totpErrors.length,
+            'is-invalid': submitted && errors.has('totpToken'),
           }"
           v-model="totpToken"
-          @input="$v.totpToken.$touch()"
           :placeholder="messages.field_label_totp_code"
         />
-        <span
-          class="form-error"
-          v-if="submitted && totpErrors && totpErrors.length"
-        >
-          {{ totpErrors.join(', ') }}
-        </span>
         <div
           v-if="submitted && errors.has('totpToken')"
           class="invalid-feedback d-block"
@@ -106,19 +85,11 @@
         <Input
           class="form-control"
           :class="{
-            'is-invalid':
-              submitted && unlockKeyErrors && unlockKeyErrors.length,
+            'is-invalid': submitted && errors.has('unlockKey'),
           }"
           v-model="unlockKey"
-          @input="$v.unlockKey.$touch()"
           :placeholder="messages.field_label_unlock_key"
         />
-        <span
-          class="form-error"
-          v-if="submitted && unlockKeyErrors && unlockKeyErrors.length"
-        >
-          {{ unlockKeyErrors.join(', ') }}
-        </span>
         <div
           v-if="submitted && errors.has('unlockKey')"
           class="invalid-feedback d-block"
@@ -200,41 +171,6 @@ export default {
       'resetPasswordMessageSent',
     ]),
     ...mapState('system', ['configs', 'messages']),
-
-    emailErrors() {
-      const errors = [];
-      if (!this.$v.email.$dirty) return errors;
-      !this.$v.email.email && errors.push(this.messages['err_email_invalid']);
-      !this.$v.email.required &&
-        errors.push(this.messages['err_email_required']);
-      return errors;
-    },
-
-    passwordErrors() {
-      const errors = [];
-      if (!this.$v.password.$dirty) return errors;
-      !this.$v.password.minLength &&
-        errors.push(this.messages['err_password_tooShort']);
-      !this.$v.password.required &&
-        errors.push(this.messages['err_password_required']);
-      return errors;
-    },
-
-    totpErrors() {
-      const errors = [];
-      if (!this.$v.totpToken.$dirty) return errors;
-      !this.$v.totpToken.required &&
-        errors.push(this.messages['err_totpToken_invalid']);
-      return errors;
-    },
-
-    unlockKeyErrors() {
-      const errors = [];
-      if (!this.$v.unlockKey.$dirty) return errors;
-      !this.$v.unlockKey.required &&
-        errors.push(this.messages['err_unlock_required']);
-      return errors;
-    },
   },
 
   methods: {
