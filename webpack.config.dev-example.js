6e0c408113364e9ddf631db95dfcdc139bd09024
Jonathan Cobb <jonathan@kyuss.org>
2020-09-07 09:46:19 -0400

    do not commit actual webpack.config.dev.js

--- webpack.config.dev-example.js
+++ webpack.config.dev-example.js
@@ -2,15 +2,18 @@
  * Copyright (c) 2020 Bubble, Inc.  All rights reserved.
  * For personal (non-commercial) use, see license: https://getbubblenow.com/bubble-license/
  */
+
+// -------------------------------------------------------------
+// Copy this file to webpack.config.dev.js to use in development
+// -------------------------------------------------------------
+
 const webpackMerge = require('webpack-merge');
 
 module.exports = webpackMerge.merge(require('./webpack.config.js'), {
   mode: 'development',
   devServer: {
     proxy: {
-    // '/api': 'http://beta.bubv.net:8888',
-    '/api': 'http://shent-ih538-8m85f-rj97o.bubv.net'
-    // '/api': 'https://beta.bubv.net/',
-    },
+    '/api': 'http://example.com:8888'   // change this to wherever your bubble API is running
+    }
   },
 });
