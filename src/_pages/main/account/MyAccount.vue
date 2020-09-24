2175106e510c1c6ca3ae55a72bbba3d28b81641d
Jonathan Cobb <jonathan@kyuss.org>
2020-09-24 16:15:57 -0400

    fix account actions view

--- src/_pages/main/account/MyAccount.vue
+++ src/_pages/main/account/MyAccount.vue
@@ -69,13 +69,15 @@ export default {
   computed: {
     ...mapState('system', ['messages', 'configs']),
     manage_account_actions: function () {
-      const account_actions = this.configs.bubbleNode === true ? 'manage_account_node_actions' : 'manage_account_sage_actions';
-      const actions = this.messages[account_actions].split(',');
-      if (this.currentUser.admin && this.currentUser.firstAdmin) {
-        const delete_index = actions.indexOf('delete');
-        return delete_index === -1 ? actions : actions.splice(delete_index,1);
+      if (this.messages) {
+        const account_actions = this.configs.bubbleNode === true ? 'manage_account_node_actions' : 'manage_account_sage_actions';
+        const actions = this.messages[account_actions].split(',');
+        if (this.currentUser.admin && this.currentUser.firstAdmin) {
+          const delete_index = actions.indexOf('delete');
+          if (delete_index !== -1) actions.splice(delete_index, 1);
+        }
+        return actions;
       }
-      return actions;
     }
   },
   methods: {
